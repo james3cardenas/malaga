3a8ea9b12bbcaee706c2f9ef4dd30df8c0c8fb85
James Cardenas <cardenasEDU@aol.com>
2013-12-07 19:17:08 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Malaga::Application.config.secret_token = 'ed8b4065be633ed98f5a9fa5d199143351597ac6017765de6b4eddc2f19be161d5c2dc4725360f065b3819b40080b3d8b8dcb4c002c2ca13a78690a3aa859cc2'
